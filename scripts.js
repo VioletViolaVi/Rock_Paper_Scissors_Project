454f14bdafe836dc7b58534e670e09d18b3f0b9c
VioletViolaVi <vivian.v18723@gmail.com>
2022-06-01 22:32:00 +0100

    add & style instruction text

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // from html
+  const wholePicGame = document.getElementById("wholePicGame");
   const rock = document.getElementById("rock");
   const paper = document.getElementById("paper");
   const scissors = document.getElementById("scissors");
@@ -8,21 +9,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const optionArr = [rock, paper, scissors];
 
   function selectingOptions() {
-    // selects btns individually
+    // selects btn individually
     optionArr.forEach((chosenBtn) => {
       chosenBtn.addEventListener("click", () => {
         if (chosenBtn.id === "rock") {
           selectedImg.src = "images/rocks.avif";
           selectedImg.alt = "rock picture";
+          wholePicGame.style.display = "block";
         } else if (chosenBtn.id === "paper") {
           selectedImg.src = "images/paper.avif";
           selectedImg.alt = "paper picture";
+          wholePicGame.style.display = "block";
         } else if (chosenBtn.id === "scissors") {
           selectedImg.src = "images/scissors.avif";
           selectedImg.alt = "scissors picture";
+          wholePicGame.style.display = "block";
         }
       });
     });
   }
   selectingOptions();
 });
+
+// may need while loop to cont. playing
+// have model appear to show score each time
