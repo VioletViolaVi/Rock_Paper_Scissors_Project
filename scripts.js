726584b022e81794d0ac19c71740dcbdbae6e8d9
VioletViolaVi <vivian.v18723@gmail.com>
2022-06-01 22:40:12 +0100

    create function body for computer choice

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // from html
   const wholePicGame = document.getElementById("wholePicGame");
   const paraText = document.getElementById("paraText");
+
   const rock = document.getElementById("rock");
   const paper = document.getElementById("paper");
   const scissors = document.getElementById("scissors");
@@ -13,7 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // selects btn individually
     optionArr.forEach((chosenBtn) => {
       chosenBtn.addEventListener("click", () => {
-        wholePicGame.style.display = "block";
+        wholePicGame.style.display = "block"; // shows imgs
+        computerChoice(); // shows computer's choice
         paraText.style.display = "none";
         if (chosenBtn.id === "rock") {
           selectedImg.src = "images/rocks.avif";
@@ -29,7 +31,14 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
   selectingOptions();
+
+  function computerChoice() {
+    console.log("computer's choice");
+  }
 });
 
 // may need while loop to cont. playing
 // have model appear to show score each time
+// randomise computer choices
+// modal for "you win" & "you lose"
+// - can include score if wanted
