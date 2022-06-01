1d580a9bf9122c040804e4b61c2c61c7051db5fb
VioletViolaVi <vivian.v18723@gmail.com>
2022-06-01 22:09:26 +0100

    use js to change imgs when respective btns clicked

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("hello world");
+  // from html
+  const rock = document.getElementById("rock");
+  const paper = document.getElementById("paper");
+  const scissors = document.getElementById("scissors");
+  const selectedImg = document.getElementById("selectedImg");
+
+  const optionArr = [rock, paper, scissors];
+
+  function selectingOptions() {
+    // selects btns individually
+    optionArr.forEach((chosenBtn) => {
+      chosenBtn.addEventListener("click", () => {
+        if (chosenBtn.id === "rock") {
+          selectedImg.src = "images/rocks.avif";
+          selectedImg.alt = "rock picture";
+        } else if (chosenBtn.id === "paper") {
+          selectedImg.src = "images/paper.avif";
+          selectedImg.alt = "paper picture";
+        } else if (chosenBtn.id === "scissors") {
+          selectedImg.src = "images/scissors.avif";
+          selectedImg.alt = "scissors picture";
+        }
+      });
+    });
+  }
+  selectingOptions();
 });
